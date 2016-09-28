b8d00bbd17cd04716ece11a29ea65129e2b2d2fd
KuristinaMakise <duquesne.alexandre.l@gmail.com>
2016-09-28 10:47:44 +0200

    définition de Point

--- Trajectoire-des-bulles/src/Point.java
+++ Trajectoire-des-bulles/src/Point.java
@@ -1,3 +1,28 @@
-public class Point {
-
+public class Point 
+{
+	private double x;
+	private double y;
+	private double z;
+	
+	public Point(double x, double y, double z)
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	public double getX()
+	{
+		return this.x;
+	}
+	
+	public double getY()
+	{
+		return this.y;
+	}
+	
+	public double getZ()
+	{
+		return this.z;
+	}
 }
