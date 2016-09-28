12b68597846023295280757c79c1b895f5e18d3b
KuristinaMakise <duquesne.alexandre.l@gmail.com>
2016-09-28 11:30:07 +0200

    ajout

--- Trajectoire-des-bulles/src/Backtracking.java
+++ Trajectoire-des-bulles/src/Backtracking.java
@@ -1,3 +1,16 @@
 public class Backtracking {
 
+	public double calculAngle(double longueur)
+	{
+		double res = 0;
+		return res;
+	}
+	
+	public double calculDistance(Point a, Point b)
+	{
+		double res = 0;
+		Math.sqrt(Math.pow((b.getX() - a.getX())));
+		
+		return res;
+	}
 }
