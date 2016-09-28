0da053e5262449681c99519050d45165731204d5
Roguelearg <lebasketteurdu76@hotmail.fr>
2016-09-28 11:43:45 +0200

    voisin

--- Trajectoire-des-bulles/src/Backtracking.java
+++ Trajectoire-des-bulles/src/Backtracking.java
@@ -15,11 +15,12 @@ public class Backtracking
 		
 	}
 	
+	
 	/**
 	 * A partir d'un fichier, on crée l'arraylist de points.
 	 * Chaque point a des coordonnées x y z.
 	 */
-	public void lectureFichier(String nomFichier)
+	public void creationGraphe(String nomFichier)
 	{
 	    FileInputStream fis = null;
 	    InputStreamReader isr = null;
@@ -68,35 +69,23 @@ public class Backtracking
 	    }
 	}
 	
-	public double convertStringEnDouble(String texte)
+	public convertStringEnDouble(String texte)
 	{
 		String[] split = texte.split("e");
 		double a = Double.parseDouble(split[0]);
-		double b = Double.parseDouble(split[1].substring(1, split[1].length()-1));
-		if(split[1].startsWith("+"))
-		{
-			return Math.pow(a,-b);
-		}
-		return Math.pow(a,-b);
-	}
-<<<<<<< HEAD
-	
-=======
-	
-	public double calculAngle(Point p1, Point p2, Point p3)
-	{
-		double res = 0;
-		
-		
-		
-		return res;
 	}
 	
-	public double calculDistance(Point a, Point b)
-	{
-		double res = Math.sqrt(Math.pow((b.getX() - a.getX()), 2) + Math.pow((b.getY() - a.getY()), 2) + Math.pow((b.getZ() - a.getZ()), 2));
-
-		return res;
-	}
->>>>>>> branch 'master' of https://github.com/Specvey/Trajectoire-des-bulles.git
+	public ArrayList<Point> voisin(Point pO) {
+		 
+		  ArrayList<Point> voisinage = new ArrayList<Point>();
+		  
+		  for(Point pV : lesPoints){
+			  if(pV.equals(pO))
+		  }
+		  
+		  
+		  return voisinage;
+		  
+		 }
+	
 }
