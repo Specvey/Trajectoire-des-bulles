39edd68d8aa1f5816ad4e16e385ee268c6b9006a
Specvey <specvey@gmail.com>
2016-09-28 12:12:07 +0200

    lecture des points OK

--- Trajectoire-des-bulles/src/Main.java
+++ Trajectoire-des-bulles/src/Main.java
@@ -2,6 +2,7 @@ public class Main
 {
 	public static void main(String args[])
 	{
-		// Backtracking bt = new Backtracking();
+		Backtracking bt = new Backtracking("data/norma_N5_tau4_dt2_delai820_000000.txt");
+		// bt.afficherLesPoints();
 	}
 }
