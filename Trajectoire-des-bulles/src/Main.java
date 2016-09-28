4e9bfb59abadbdb9a1ccb598dea18e9dbc72a8cf
KuristinaMakise <duquesne.alexandre.l@gmail.com>
2016-09-28 11:46:39 +0200

    retour

--- Trajectoire-des-bulles/src/Main.java
+++ Trajectoire-des-bulles/src/Main.java
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-public class Main {
-	
-
-=======
 public class Main
 {
 	public static void main(String args[])
 	{
 		// Backtracking bt = new Backtracking();
 	}
->>>>>>> branch 'master' of https://github.com/Specvey/Trajectoire-des-bulles.git
 }
