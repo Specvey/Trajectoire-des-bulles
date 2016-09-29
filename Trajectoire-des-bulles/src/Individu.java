f5448c974744c89b420a6ecd43aa5a1433ff0238
Xavier <Xavier@DESKTOP-R29J4UN>
2016-09-29 12:02:27 +0200

    YOLO

--- Trajectoire-des-bulles/src/Individu.java
+++ Trajectoire-des-bulles/src/Individu.java
@@ -1,6 +1,14 @@
+import java.util.ArrayList;
 
 public class Individu {
 	
-	private ArrayList<Trajectoire>;
+	private ArrayList<Trajectoire> trajectoires;
+	private double fitness;
+	
+	public Individu(ArrayList<Point> points, boolean aleatoire){
+		
+	}
+	
+	public int get
 
 }
