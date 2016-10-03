5dba543d00680cb90afb2577e5484f5f303c1ce5
KuristinaMakise <duquesne.alexandre.l@gmail.com>
2016-10-03 09:56:24 +0200

    modif

--- Trajectoire-des-bulles/src/BacktrackingSpecvey.java
+++ Trajectoire-des-bulles/src/BacktrackingSpecvey.java
@@ -250,13 +250,11 @@ public class BacktrackingSpecvey
             	meilleureSerie = serieEnCours;
             }
             
-            
             trajectoires.add(trajectoires.get(0));
             trajectoires.remove(0);
-   
         }
             
-        return trajectoires;
+        return meilleureSerie;
     }
 	
 	
