4f441487eb99f78853543c5263ceec88c5551a29
KuristinaMakise <duquesne.alexandre.l@gmail.com>
2016-10-10 11:18:29 +0200

    test

--- Trajectoire-des-bulles/src/Main.java
+++ Trajectoire-des-bulles/src/Main.java
@@ -4,16 +4,22 @@ public class Main
 {
 	public static void main(String args[])
 	{
-		//Backtracking bt = new Backtracking("data/norma_N5_tau4_dt2_delai820_000000.txt");
+		Backtracking bt = new Backtracking("data/norma_N5_tau4_dt2_delai820_000000.txt");
 		// bt.afficherLesPoints();
 		
 		BacktrackingSpecvey bs = new BacktrackingSpecvey("data/norma_N5_tau4_dt2_delai820_000000.txt");
 		
-		ArrayList<Point[]> trajectoires = bs.deroulement();
+		/*ArrayList<Point[]> trajectoires = bs.deroulement();
 		
-		ArrayList<Point[]> resultat = bs.nettoyageDePrintemps(trajectoires);
+		ArrayList<Point[]> resultat = bs.nettoyageDePrintemps(trajectoires);*/
 		
-		System.out.println(resultat.size());
+		ArrayList<Point[]> trajectoiresBt = bt.deroulement();
+		ArrayList<Point[]> trajectoiresBs = bs.deroulement();
+		
+		//ArrayList<Point[]> resultat = bt.ne
+		
+		System.out.println(trajectoiresBt.size());
+		System.out.println(trajectoiresBs.size());
 		
 	}
 
